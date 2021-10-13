70ba9a29184fc430a3749b0c422e1469ac84efc5
Luismosca <luismosca@gmail.com>
2021-10-12 22:18:40 -0300

    confotmao

--- Navegable/Pagos/FrmPagosDeudaAño.vb
+++ Navegable/Pagos/FrmPagosDeudaAño.vb
@@ -1,9 +1,3 @@
 ﻿Public Class FrmPagosDeudaAño
-    Private Sub FrmPagosDeudaAño_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-    End Sub
-
-    Private Sub Label4_Click(sender As Object, e As EventArgs) Handles Label4.Click
-
-    End Sub
 End Class
